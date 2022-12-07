9b5168bddd0f8b1a052d437b40e41852d628f1e3
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-12-08 00:33:45 +0300

    NbrbCurrencyConversionService refactored

--- src/main/java/by/smirnov/currencyconverterbot/service/NbrbCurrencyConversionService.java
+++ src/main/java/by/smirnov/currencyconverterbot/service/NbrbCurrencyConversionService.java
@@ -1,15 +1,22 @@
 package by.smirnov.currencyconverterbot.service;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import by.smirnov.currencyconverterbot.entity.Currency;
-import lombok.SneakyThrows;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class NbrbCurrencyConversionService implements CurrencyConversionService {
+
+    public static final String NBRB_RATES_URL = "https://www.nbrb.by/api/exrates/rates/";
+    public static final String NBRB_KEY_RATE = "Cur_OfficialRate";
+    public static final String NBRB_KEY_SCALE = "Cur_Scale";
+    public static final String GET_METHOD = "GET";
+
     @Override
     public double getConversionRatio(Currency original, Currency target) {
         double originalRate = getRate(original);
@@ -17,24 +24,42 @@ public class NbrbCurrencyConversionService implements CurrencyConversionService 
         return originalRate / targetRate;
     }
 
-    @SneakyThrows
     private double getRate(Currency currency) {
         if (currency == Currency.BYN) {
             return 1;
         }
-        URL url = new URL("https://www.nbrb.by/api/exrates/rates/" + currency.getId());
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        JSONObject json = new JSONObject(response.toString());
-        double rate = json.getDouble("Cur_OfficialRate");
-        double scale = json.getDouble("Cur_Scale");
+        JSONObject json = getRateJson(getRateUrl(currency));
+        double rate = json.getDouble(NBRB_KEY_RATE);
+        double scale = json.getDouble(NBRB_KEY_SCALE);
+
         return rate / scale;
+    }
+
+    private URL getRateUrl(Currency currency) {
+        try {
+            return new URL(NBRB_RATES_URL + currency.getId());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private JSONObject getRateJson(URL url) {
+        StringBuilder response = new StringBuilder();
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(GET_METHOD);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject(response.toString());
     }
 }
