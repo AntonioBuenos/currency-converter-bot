0d6281c03e6292bba070c1381f3e397ad3d98598
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2023-01-04 01:09:46 +0300

    Rate entity added

--- src/main/java/by/smirnov/currencyconverterbot/entity/Rate.java
+++ src/main/java/by/smirnov/currencyconverterbot/entity/Rate.java
@@ -6,24 +6,42 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 @ToString
+@Entity
+@Table(name = "rate")
 public class Rate {
 
+    @Id
     @JsonSetter("Cur_ID")
+    @Column(name = "id")
     private Long id;
+
     @JsonSetter("Date")
+    @Column(name = "date")
     private Timestamp date;
+
     @JsonSetter("Cur_Abbreviation")
+    @Column(name = "abbreviation")
     private String abbreviation;
+
     @JsonSetter("Cur_Scale")
+    @Column(name = "scale")
     private Long scale;
+
     @JsonSetter("Cur_Name")
+    @Column(name = "name")
     private String name;
+
     @JsonSetter("Cur_OfficialRate")
+    @Column(name = "official_rate")
     private Double officialRate;
 }
