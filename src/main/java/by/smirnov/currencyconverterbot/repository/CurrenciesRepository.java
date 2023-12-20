fb0335dcfd42e50ae4cfc7e5ba05e18e84019aa9
Anton Smirnov <anton.smirnov@enable-u.com>
2023-12-20 11:58:18 +0300

    Params set to CurrenciesRepository

--- src/main/java/by/smirnov/currencyconverterbot/repository/CurrenciesRepository.java
+++ src/main/java/by/smirnov/currencyconverterbot/repository/CurrenciesRepository.java
@@ -3,6 +3,7 @@ package by.smirnov.currencyconverterbot.repository;
 import by.smirnov.currencyconverterbot.entity.Currency;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.sql.Timestamp;
 import java.util.Optional;
@@ -11,5 +12,5 @@ public interface CurrenciesRepository extends
         CrudRepository<Currency, Long> {
 
     @Query("select c from Currency c where c.abbreviation = :abbreviation and c.dateStart < :date and c.dateEnd > :date")
-    Optional<Currency> getActualCurrency(String abbreviation, Timestamp date);
+    Optional<Currency> getActualCurrency(@Param("abbreviation") String abbreviation, @Param("date") Timestamp date);
 }
