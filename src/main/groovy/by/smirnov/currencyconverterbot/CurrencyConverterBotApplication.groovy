15a5483aeca9eeaf8d90709f4b3ffc59be928bd0
Anton Smirnov <anton.smirnov@enable-u.com>
2023-12-21 10:25:53 +0300

    Main app class migrated to Groovy

---
+++ src/main/groovy/by/smirnov/currencyconverterbot/CurrencyConverterBotApplication.groovy
@@ -0,0 +1,12 @@
+package by.smirnov.currencyconverterbot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CurrencyConverterBotApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterBotApplication.class, args)
+    }
+}
