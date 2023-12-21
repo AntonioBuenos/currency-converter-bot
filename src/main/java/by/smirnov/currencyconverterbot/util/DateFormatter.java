a2655f5f77eb62d8cd041467ff3af66b187be679
Anton Smirnov <anton.smirnov@enable-u.com>
2023-12-21 08:20:27 +0300

    Locale added to DateFormatter

--- src/main/java/by/smirnov/currencyconverterbot/util/DateFormatter.java
+++ src/main/java/by/smirnov/currencyconverterbot/util/DateFormatter.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @UtilityClass
 public class DateFormatter {
@@ -11,7 +12,7 @@ public class DateFormatter {
     public static final String DATE_PATTERN = "d MMMM yyyy";
 
     public static String formatDate(LocalDate date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN, Locale.of("ru", "RU"));
         return date.format(formatter);
     }
 }
